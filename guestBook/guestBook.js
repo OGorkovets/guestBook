095f1c5100611f96420065d36df0e3b87d53e7e6
ogorkovets <ogorkovets@mail.greenriver.edu>
2016-01-31 14:46:18 -0800

    modified version of guestbook

--- guestBook/guestBook.js
+++ guestBook/guestBook.js
@@ -1,23 +1,58 @@
+Messages = new Mongo.Collection("messages");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+	//subscribe client to Mongo DB
+	Meteor.subscribe("messages");
+	//after creating helpers we can acces name and message fields in any of our project files
+	Template.guestBook.helpers({
+		"messages" : function(){
+		return Messages.find({}) || {};
+			//return all message objects, or an empty object if DB is invalid
+		}
+	});
+	
+	//access gusetBook template object from html fileCreatedDate
+	//and use built-in method events
+  Template.guestBook.events(
+		{
+			"submit form": function(event){
+				event.preventDefault();
+				
+				//this is jQeury statement 
+				var messageBox = $(event.target).find('textarea[name=guestMessage]');
+				
+				var messageText = messageBox.val();
+				
+				var nameBox = $(event.target).find('input[name=guestName]');
+				
+				var nameText = nameBox.val();
+				
+				
+				Messages.insert(
+					{
+							name: nameText,
+							message: messageText,
+							createdOn: Date.now()
+					}
+				);
+				messageBox.val('');
+				nameBox.val('');
+				
+				//alert("Name is " + nameText + ", msg is " + nameText);
+				//alert("you clicked submit.");
+			}
+			
+			
+		}
+	);
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+  
+  Meteor.publish("messages", function(){
+	  return Messages.find({}, {sort: {createdOn: - 1}}) || {};
   });
 }
